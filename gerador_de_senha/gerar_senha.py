9f7498cab4df085799239974e43073a65ee56170
Ari Pontes <arypontys@hotmail.com>
2022-06-26 15:23:00 -0300

    arquivo para gerar senhas aleatórias

---
+++ gerador_de_senha/gerar_senha.py
@@ -0,0 +1,31 @@
+# Gerador de senhas aleatórias
+import random
+
+def gerar_senha():
+    alfabeto = ['q','w','e','r','t','y','u','i','o','p','a','s','d',
+    'f','g','h','j','k','l','ç','z','x','c','v','b','n','m','Q',
+    'W','E','R','T','Y','U','I','O','P','A','S','D','F','G',
+    'H','J','K','L','Ç','Z','X','C','V','B','N','M']
+    numeros = ['0','1','2','3','4','5','6','7','8','9']
+    simbolos = ['!','@','#','$','%','&','*','(',')','+']
+    lista_senha =[]
+    senha = []
+    
+    for l in range(0, 8):
+        lista_senha.append(random.choices(alfabeto))
+    for l in range(0, 2):
+        lista_senha.append(random.choices(numeros))
+    for l in range(0, 2):
+        lista_senha.append(random.choices(simbolos))
+    
+    random.shuffle(lista_senha)
+
+    for l in lista_senha:
+        for i in l:
+            senha.append(i)
+        a = ''.join(senha)
+
+    return a
+
+senha = gerar_senha()
+print(senha)
